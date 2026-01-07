85ef417da1bb15e523b57caad9c3d9835eb4c141
i2w3 <45359144+i2w3@users.noreply.github.com>
2026-01-07 09:21:58 +0800

    update plot

--- test_gvlm_dataset.py
+++ test_gvlm_dataset.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import time
-from typing import List, Union
+from typing import List, Union, Optional
 
 import cv2
 import matplotlib.pyplot as plt
@@ -16,7 +16,7 @@ def parse_args():
     parser.add_argument("--dataset_path", "-dp", type=str, default="./res/data/GVLM_CD",
                         help="GVLM_CD 数据集路径")
     parser.add_argument("--sample_name", "-sn", type=str, default="A Luoi_Vietnam",
-                        help="数据集路径下子样本名称")
+                        help="数据集路径下子样本名称，默认读取 ./res/data/GVLM_CD/A Luoi_Vietnam 文件夹下的数据")
     parser.add_argument("--cut_size", "-cs", type=int, default=None,
                         help="裁剪子图的大小，默认不裁剪使用全图")
     parser.add_argument("--enable_force_grouth", "-efg", action="store_true",
@@ -30,6 +30,47 @@ def parse_args():
     parser.add_argument("--config_path", type=str, default="./config.json",
                         help="配置文件路径")
     return parser.parse_args()
+
+
+def plot_data(args:argparse.Namespace, config:dict, img_data:GVLM_Sample, mask_data:SE2020OUTPUT, save_str:Optional[str] = None):
+    '''绘制图像数据和预测结果
+    '''
+    fig, axes = plt.subplots(2, 3, figsize=(15, 10))
+    ## Row 1
+    axes[0,0].imshow(cv2.cvtColor(img_data.img_A, cv2.COLOR_BGR2RGB))
+    axes[0,0].set_title("img_A")
+    axes[0,0].axis('off')
+    axes[0,1].imshow(cv2.cvtColor(img_data.img_B, cv2.COLOR_BGR2RGB))
+    axes[0,1].set_title("img_B")
+    axes[0,1].axis('off')
+    axes[0,2].imshow(img_data.img_ref, cmap='gray')
+    axes[0,2].set_title("ref")
+    axes[0,2].axis('off')
+    ## Row 2
+    axes[1,0].imshow(over_leap(img_data.img_A, mask_data.mask_1))
+    axes[1,0].set_title("mask_A")
+    axes[1,0].axis('off')
+    axes[1,1].imshow(over_leap(img_data.img_B, mask_data.mask_2))
+    axes[1,1].set_title("mask_B")
+    axes[1,1].axis('off')
+    if args.enable_force_grouth:
+        axes[1,2].imshow(mask_data.mask_bin, cmap='gray')
+        axes[1,2].set_title("mask_bin")
+        axes[1,2].axis('off')
+    else:
+        # plot lengends for all classes
+        legend_elements = []
+        for i, class_name in enumerate(config["SE2020"]["classes_name"]):
+            color = np.array(config["SE2020"]["classes_cmap"][i]) / 255.0
+            legend_elements.append(plt.Line2D([0], [0], marker='s', color='w', label=class_name,
+                                              markerfacecolor=color, markersize=10))
+        axes[1,2].axis('off')
+        axes[1,2].legend(handles=legend_elements, loc='center', fontsize='large')
+
+    plt.tight_layout()
+    if save_str is not None:
+        plt.savefig(save_str)
+    return fig
 
 
 def calc_mIoU(pred_mask:np.ndarray, true_mask:np.ndarray, cls_list:List[int]) -> float:
@@ -82,16 +123,16 @@ if __name__ == "__main__":
     model = SE2020(config["SE2020"])
     dataset = GVLM_CDataset(args.dataset_path)
     sample = dataset.sub_gen(args.sample_name, args.cut_size, args.seed) # 随机裁剪1024x1024大小的图像块
-    sample_enhance, enchance_imgB = UAV_enchance(sample, 512)
+    # sample_enhance, enchance_imgB = UAV_enchance(sample, 512)
 
-    pred = model(sample_enhance.img_A, sample_enhance.img_B)
+    pred = model(sample.img_A, sample.img_B)
     if args.enable_force_grouth:
         pred.mask_1[pred.mask_1 != 2] = 0 # 强迫仅关注地面变化类别(id 2)
         pred.mask_2[pred.mask_2 != 2] = 0
         # 在 SE2020 中的 mask_bin，1 表示未变化，0 表示有变化，但是不包含类别信息，所以需要根据 mask_1 和 mask_2 重新计算地面变化
         pred.mask_bin = pred.mask_1 + pred.mask_2
         pred.mask_bin[pred.mask_bin != 0] = 255
-        print(f"mIoU: {calc_mIoU(pred.mask_bin, sample_enhance.img_ref, [0,255]):.4f}") # mIoU 计算仅支持强迫关注地面变化的情况
+        print(f"mIoU: {calc_mIoU(pred.mask_bin, sample.img_ref, [0,255]):.4f}") # mIoU 计算仅支持强迫关注地面变化的情况
     else:
         pred.mask_1 = Image.fromarray(pred.mask_1).convert("P")
         pred.mask_1.putpalette(model.color_map)
@@ -99,41 +140,8 @@ if __name__ == "__main__":
         pred.mask_2 = Image.fromarray(pred.mask_2).convert("P")
         pred.mask_2.putpalette(model.color_map)
 
-    fig, axes = plt.subplots(2, 3, figsize=(15, 10))
-    ## Row 1
-    axes[0,0].imshow(cv2.cvtColor(sample_enhance.img_A, cv2.COLOR_BGR2RGB))
-    axes[0,0].set_title("img_A")
-    axes[0,0].axis('off')
-    axes[0,1].imshow(cv2.cvtColor(sample_enhance.img_B, cv2.COLOR_BGR2RGB))
-    axes[0,1].set_title("img_B")
-    axes[0,1].axis('off')
-    axes[0,2].imshow(sample_enhance.img_ref, cmap='gray')
-    axes[0,2].set_title("ref")
-    axes[0,2].axis('off')
-    ## Row 2
-    axes[1,0].imshow(over_leap(sample_enhance.img_A, pred.mask_1))
-    axes[1,0].set_title("mask_A")
-    axes[1,0].axis('off')
-    axes[1,1].imshow(over_leap(sample_enhance.img_B, pred.mask_2))
-    axes[1,1].set_title("mask_B")
-    axes[1,1].axis('off')
-    if args.enable_force_grouth:
-        axes[1,2].imshow(pred.mask_bin, cmap='gray')
-        axes[1,2].set_title("mask_bin")
-        axes[1,2].axis('off')
-    else:
-        # plot lengends for all classes
-        legend_elements = []
-        for i, class_name in enumerate(config["SE2020"]["classes_name"]):
-            color = np.array(config["SE2020"]["classes_cmap"][i]) / 255.0
-            legend_elements.append(plt.Line2D([0], [0], marker='s', color='w', label=class_name,
-                                              markerfacecolor=color, markersize=10))
-        axes[1,2].axis('off')
-        axes[1,2].legend(handles=legend_elements, loc='center', fontsize='large')
-
-    plt.tight_layout()
-    # plt.savefig(f"images/gvlm_result.png")
-    # plt.show()
+    fig = plot_data(args, config, sample, pred, save_str="images/gvlm_result.png")
+    plt.show()
     
     if not args.enable_more_test:
         raise SystemExit("Only run single test!")
@@ -145,16 +153,16 @@ if __name__ == "__main__":
     for i in LAND_TYPE:
         for j in range(args.run_time):
             sample = dataset.sub_gen(i, args.cut_size, j)
-            sample_enhance, enchance_imgB = UAV_enchance(sample, 512)
+            # sample_enhance, enchance_imgB = UAV_enchance(sample, 512)
             start_time = time.time()
-            pred = model(sample_enhance.img_A, sample_enhance.img_B)
+            pred = model(sample.img_A, sample.img_B)
             if args.enable_force_grouth:
                 pred.mask_1[pred.mask_1 != 2] = 0 # 强迫仅关注地面变化类别(id 2)
                 pred.mask_2[pred.mask_2 != 2] = 0
                 # 在 SE2020 中的 mask_bin，1 表示未变化，0 表示有变化，但是不包含类别信息，所以需要根据 mask_1 和 mask_2 重新计算地面变化
                 pred.mask_bin = pred.mask_1 + pred.mask_2
                 pred.mask_bin[pred.mask_bin != 0] = 255
-                mIOU = calc_mIoU(pred.mask_bin, sample_enhance.img_ref, [0,255])
+                mIOU = calc_mIoU(pred.mask_bin, sample.img_ref, [0,255])
                 MIOU_LIST.append(mIOU)
             else:
                 pred.mask_1 = Image.fromarray(pred.mask_1).convert("P")
@@ -164,38 +172,7 @@ if __name__ == "__main__":
                 pred.mask_2.putpalette(model.color_map)
             end_time = time.time()
             TIME_LIST.append(end_time - start_time)
-            fig, axes = plt.subplots(2, 3, figsize=(15, 10))
-            ## Row 1
-            axes[0,0].imshow(cv2.cvtColor(sample_enhance.img_A, cv2.COLOR_BGR2RGB))
-            axes[0,0].set_title("img_A")
-            axes[0,0].axis('off')
-            axes[0,1].imshow(cv2.cvtColor(sample_enhance.img_B, cv2.COLOR_BGR2RGB))
-            axes[0,1].set_title("img_B")
-            axes[0,1].axis('off')
-            axes[0,2].imshow(sample_enhance.img_ref, cmap='gray')
-            axes[0,2].set_title("ref")
-            axes[0,2].axis('off')
-            ## Row 2
-            axes[1,0].imshow(over_leap(sample_enhance.img_A, pred.mask_1))
-            axes[1,0].set_title("mask_A")
-            axes[1,0].axis('off')
-            axes[1,1].imshow(over_leap(sample_enhance.img_B, pred.mask_2))
-            axes[1,1].set_title("mask_B")
-            axes[1,1].axis('off')
-            if args.enable_force_grouth:
-                axes[1,2].imshow(pred.mask_bin, cmap='gray')
-                axes[1,2].set_title("mask_bin")
-                axes[1,2].axis('off')
-            else:
-                # plot lengends for all classes
-                legend_elements = []
-                for k, class_name in enumerate(config["SE2020"]["classes_name"]):
-                    color = np.array(config["SE2020"]["classes_cmap"][k]) / 255.0
-                    legend_elements.append(plt.Line2D([0], [0], marker='s', color='w', label=class_name, markerfacecolor=color, markersize=10))
-                axes[1,2].axis('off')
-                axes[1,2].legend(handles=legend_elements, loc='center', fontsize='large')
-
-            plt.tight_layout()
-            plt.savefig(f"images/{i}-{j}.png")
+            
+            plot_data(args, config, sample, pred, save_str=f"images/{i}-{j}.png")
     print(f"Average mIoU: {np.mean(MIOU_LIST):.4f}")
     print(f"Average processing time: {np.mean(TIME_LIST):.4f} seconds")
