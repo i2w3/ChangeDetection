09cd2d01d61e3ba527751c65604dcd3c3ad2a562
i2w3 <45359144+i2w3@users.noreply.github.com>
2026-01-09 16:03:29 +0800

    update SCanNet

--- README.md
+++ README.md
@@ -21,7 +21,45 @@ pip install opencv-python-headless mmpretrain
 此外，此模型由于有自定义 cuda 算子，转换为 onnx 后与原始 pt 模型输出存在明显差异
 
 ## [SCanNet](https://github.com/DingLei14/SCanNet)
-![](./images/SCanNet.png)
+```bash
+git clone https://github.com/DingLei14/SCanNet
+cd SCanNet
+```
+```python
+import torch
+
+from models.SCanNet import SCanNet as Net
+
+model = Net(3, 7).cuda()
+model.load_state_dict(torch.load("models/SCanNet_32e_mIoU73.37_Sek23.94_Fscd63.66_OA87.86.pth"))
+model = model.cuda()
+model.eval()
+
+dummy_input1 = torch.rand(1, 3, 512, 512).cuda()
+dummy_input2 = torch.rand(1, 3, 512, 512).cuda()
+
+with torch.no_grad():
+    output_bcd, output_T1, output_T2 = model(dummy_input1, dummy_input2)
+
+print(output_bcd.shape)
+print(output_T1.shape)
+print(output_T2.shape)
+
+onnx_program = torch.onnx.export(
+    model, 
+    (dummy_input1, dummy_input2), 
+    "./SCanNet_SECOND.onnx",
+    export_params=True,
+    input_names=['input1', 'input2'],
+    output_names=['output_bin', 'output1', 'output2'],
+    opset_version=17,
+    external_data=False,
+    verbose=False
+)
+```
+SCanNet 的官方代码中，img_A 和 img_B 使用不同的 mean/std 参数，但是实测差别不大
+![普通RGB标准化参数](./images/SCanNet.png)
+![官方双参数](./images/SCanNet2.png)
 
 # more model
 [open-cd](https://github.com/likyoo/open-cd) base on MMLab Toolkits
--- src/model/SCanNet.py
+++ src/model/SCanNet.py
@@ -3,12 +3,27 @@ from typing import List
 import numpy as np
 
 from ..ort_runner import ORTRunner, FinalResult
-from ..ort_runner import sigmoid
+from ..ort_runner import sigmoid, logger, cv2, time
 
 
 class SCanNet(ORTRunner):
     def __init__(self, config:dict):
         super().__init__(config)
+
+    def preProcess(self, img:np.ndarray, use_one:bool = True) -> np.ndarray:
+        shape = img.shape[:2]
+        input_size = (self.config["model_size"], self.config["model_size"])
+        if shape[0] != self.config["model_size"] or shape[1] != self.config["model_size"]:
+            img = cv2.resize(img, input_size)
+        if use_one:
+            mean_array = np.array(self.config["model_mean"], np.float32).reshape(1,3,1,1)
+            std_array  = np.array(self.config["model_std"] , np.float32).reshape(1,3,1,1)
+        else:
+            mean_array = np.array(self.config["model_mean2"], np.float32).reshape(1,3,1,1)
+            std_array  = np.array(self.config["model_std2"] , np.float32).reshape(1,3,1,1)
+        blob = cv2.dnn.blobFromImage(img, scalefactor=1.0,size=input_size,mean=(0,0,0),swapRB=True,ddepth=cv2.CV_32F) # only swapRB and scale
+        blob = (blob - mean_array) / std_array
+        return blob
 
     def postProcess(self, outputs_blob:List[np.ndarray]) -> FinalResult:
         output_bin, output1, output2,  = outputs_blob
@@ -22,3 +37,22 @@ class SCanNet(ORTRunner):
         np_pb = np.argmax(output2, axis=1).astype(np.uint8) * mask_bin
         
         return FinalResult(mask_bin=mask_bin[0], mask_1=np_pa[0], mask_2=np_pb[0])
+    
+    def infer(self, image1: np.ndarray, image2: np.ndarray):
+        start_time = time.time()
+        input1_blob = self.preProcess(image1)
+        input2_blob = self.preProcess(image2, use_one=False)
+        preProcess_time = time.time() - start_time
+
+        start_time = time.time()
+        outputs_blob:list = self.session.run(self.outputs, {"input1": input1_blob, "input2": input2_blob})
+        inference_time = time.time() - start_time
+        
+        start_time = time.time()
+        result = self.postProcess(outputs_blob)
+        postProcess_time = time.time() - start_time
+        
+        logger("info", f"Average Pre-Process Time:  {preProcess_time*1000:.2f} ms")
+        logger("info", f"Average Inference Time:   {inference_time*1000:.2f} ms")
+        logger("info", f"Average Post-Process Time: {postProcess_time*1000:.2f} ms")
+        return result
